b271c01ddd383ced86a5d5995eb317b241b7f30d
Augie Bren <Augie>
2022-08-25 14:40:23 -0500

    made token

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require('../models')
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -14,10 +15,32 @@ router.post('/', async (req, res) => {
             message: `Could not find user with the provided creditals`
         })
     } else {
-        res.json({ user })
-        console.log("yes you are loged in")
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        res.json({ user: user, token: result.value })
     }
-    console.log(user, "authjs")
+})
+
+router.get('/profile', async (req, res) => {
+    try {
+        // Split the auth header
+        const [authenticationMethod, token] = req.headers.authorization.split('')
+        // handle Bearer
+        if (authenticationMethod == 'Bearer') {
+            // Decode JWT
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+            // Get the logged in user id
+            const { id } = result.value
+            // Find the user by id
+            let user = await user.findOne({
+                where: {
+                    userId: id
+                }
+            })
+            res.json(user)
+        }        
+    } catch {
+        res.json(null)
+    }
 })
 
 module.exports = router
